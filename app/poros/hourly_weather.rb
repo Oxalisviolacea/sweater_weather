13521be4e2ed7473a47f77221b1d1852c567275c
oxalisviolacea <hannadavis09@gmail.com>
2021-01-17 18:23:03 -0700

    Make hourly weather objects

---
+++ app/poros/hourly_weather.rb
@@ -0,0 +1,18 @@
+class HourlyWeather
+  attr_reader :time, :temp, :wind_speed, :wind_direction, :conditions, :icon
+  def initialize(attributes)
+    @time = Time.at(attributes[:dt]).strftime('%H:%M:%S')
+    @temp = attributes[:temp].to_f
+    @wind_speed = attributes[:wind_speed]
+    @wind_direction = cardinal_direction(attributes[:wind_deg])
+    @conditions = attributes[:weather].first[:description]
+    @icon = attributes[:weather].first[:icon]
+  end
+
+  def cardinal_direction(deg)
+    calculation = ((deg / 22.5) + 0.5).round(0)
+    conversion = {1=> 'N', 2=>  'NNE', 3=>  'NE', 4=>  'ENE', 5=>  'E', 6=>  'ESE', 7=>  'SE', 8=>  'SSE', 9=>  'S', 
+      10=>  'SSW', 11=>  'SW', 12=>  'WSW', 13=>  'W', 14=>  'WNW', 15=>  'NW', 16=>  'NNW', 17=>  'N'}
+    conversion[calculation]
+  end
+end
---
+++ spec/poros/hourly_weather_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe HourlyWeather do
+  it 'exists' do
+    attrs = { dt: 1_610_920_800,
+              temp: 281.77,
+              feels_like: 277.62,
+              pressure: 829,
+              humidity: 28,
+              dew_point: 265.28,
+              uvi: 0.62,
+              clouds: 75,
+              visibility: 10_000,
+              wind_speed: 1.69,
+              wind_deg: 162,
+              weather: [{ id: 803, main: 'Clouds', description: 'broken clouds', icon: '04d' }],
+              pop: 0 }
+
+    hourly_weather = HourlyWeather.new(attrs)
+
+    expect(hourly_weather).to be_a HourlyWeather
+    expect(hourly_weather.time).to eq('15:00:00')
+    expect(hourly_weather.temp).to eq(281.77)
+    expect(hourly_weather.wind_speed).to eq(1.69)
+    expect(hourly_weather.wind_direction).to eq('SSE')
+    expect(hourly_weather.conditions).to eq('broken clouds')
+    expect(hourly_weather.icon).to eq('04d')
+  end
+end
