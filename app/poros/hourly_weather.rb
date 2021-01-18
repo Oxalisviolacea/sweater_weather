04ec0a3945d252cf1b780a34ab495a8d7d6735d6
oxalisviolacea <hannadavis09@gmail.com>
2021-01-17 21:04:41 -0700

    Add info to string

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -14,6 +14,6 @@ class HourlyWeather
     calculation = ((deg / 22.5) + 0.5).round(0)
     conversion = { 1 => 'N', 2 => 'NNE', 3 => 'NE', 4 => 'ENE', 5 => 'E', 6 => 'ESE', 7 => 'SE', 8 => 'SSE', 9 => 'S',
                    10 => 'SSW', 11 => 'SW', 12 => 'WSW', 13 => 'W', 14 => 'WNW', 15 => 'NW', 16 => 'NNW', 17 => 'N' }
-    conversion[calculation]
+    "from #{conversion[calculation]}"
   end
 end
