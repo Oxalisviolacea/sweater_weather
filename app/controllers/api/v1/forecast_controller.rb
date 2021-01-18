159c7929abc9798d4e1b3a51d1e4889d6049cc74
oxalisviolacea <hannadavis09@gmail.com>
2021-01-17 21:03:41 -0700

    Update show method to send data to serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    WeatherFacade.forecasts(params[:location])
+    location = params[:location]
+    current = WeatherFacade.current_weather(location)
+    daily = WeatherFacade.daily_weather(location)
+    hourly = WeatherFacade.hourly_weather(location)
+    render json: ForecastSerializer.format_data(current, daily, hourly)
   end
 end
