96c1ac10da2ddfd9bbe24ad34d1e70a0d3a5cd65
oxalisviolacea <hannadavis09@gmail.com>
2021-01-17 18:23:42 -0700

    Add openweather api call

---
+++ app/services/weather_service.rb
@@ -0,0 +1,10 @@
+class WeatherService
+  def self.forecasts(lat_lng)
+    response = conn.get("/data/2.5/onecall?lat=#{lat_lng[:lat]}&lon=#{lat_lng[:lng]}&exclude=alerts,minutely&appid=3ccdb7c078b5a11a2a101756e8b198d4&unites=imperial")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: "https://api.openweathermap.org")
+  end
+end
