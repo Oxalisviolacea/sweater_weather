0e168ac6fe81f6f01146b820aa130229ea7c70e2
oxalisviolacea <hannadavis09@gmail.com>
2021-01-17 18:23:25 -0700

    Add mapquest api call

---
+++ app/services/location_service.rb
@@ -0,0 +1,11 @@
+class LocationService
+  def self.find_lat_lng(city_state)
+    response = conn.get("/geocoding/v1/address?key=4q5RAFc0JgWH8VeU2rfymOtjFjQvLH4X&location=#{city_state}&thumbMaps=false")
+    json = JSON.parse(response.body, symbolize_names: true)
+    json[:results].first[:locations].first[:latLng]
+  end
+
+  def self.conn
+    Faraday.new(url: "http://www.mapquestapi.com")
+  end
+end
---
+++ spec/services/location_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe LocationService do
+  context "instance methods" do
+    it "a latitude and longitude for given city and state" do
+        search = LocationService.find_lat_lng('denver,co')
+        expect(search).to be_a Hash
+
+        expect(search).to have_key :lat
+        expect(search[:lat]).to be_a(Float)
+
+        expect(search).to have_key :lng
+        expect(search[:lng]).to be_a(Float)
+    end
+  end
+end
