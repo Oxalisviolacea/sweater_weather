7cf75c9e1e4779f86f57313bee99bab3d2aa6ba2
oxalisviolacea <hannadavis09@gmail.com>
2021-01-19 11:48:09 -0700

    Format outputs

--- app/controllers/api/v1/business_destinations_controller.rb
+++ app/controllers/api/v1/business_destinations_controller.rb
@@ -1,22 +1,30 @@
 require 'date'
+include ActionView::Helpers::DateHelper
 
 class Api::V1::BusinessDestinationsController < ApplicationController
   def index
     start_location = params[:start_location]
     end_location = params[:end_location]
 
+    a = end_location.split(",")
+    a[1].upcase!
+    a[0].capitalize!
+  end_location_formatted = a.join(", ")
 
     drive_time = LocationService.drive_time(start_location, end_location)
-    seconds = drive_time % 60
-    minutes = (drive_time / 60) % 60
-    hours = drive_time / (60 * 60)
-    time = format("%02d:%02d:%02d", hours, minutes, seconds)
+    drive_time_in_s = drive_time[:route][:time]
+    drive_time_hms = drive_time[:route][:formattedTime]
+
+    drive_time_words = drive_time_hms.to_datetime.strftime("%H hours, %M minutes, and %S seconds")
     category = params[:food]
     term = 'food'
-    arrival_time = DateTime.parse((Time.now + drive_time).to_s).to_time.to_i
+    arrival_time = DateTime.parse((Time.now + drive_time_in_s).to_s).to_time.to_i
 
-    daily_weather = WeatherFacade.daily_weather(end_location)
-
+    hourly_weather = WeatherFacade.hourly_weather(end_location)
+    destination_weather = hourly_weather.min_by do |hw|
+      hw.time.to_datetime.to_i - arrival_time
+    end
+    
     conn = Faraday.new('https://api.yelp.com') do |f|
       f.headers['Authorization'] = ENV['YELP_API_KEY']
     end
@@ -30,6 +38,6 @@ class Api::V1::BusinessDestinationsController < ApplicationController
     
     parsed_json = JSON.parse(response.body, symbolize_names: true)
     business = parsed_json[:businesses].first
-    render json: BusinessDestinationsSerializer.format_data(time, business, daily_weather, end_location)
+    render json: BusinessDestinationsSerializer.format_data(drive_time_words, end_location_formatted, business, destination_weather)
   end
 end
