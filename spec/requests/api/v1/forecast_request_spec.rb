b2299e921ed1c6b192630567338268256443ac5f
oxalisviolacea <hannadavis09@gmail.com>
2021-01-18 17:46:35 -0700

    Change describe block wording

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Forecast API" do
+describe "Forecast API Endpoint" do
   it "it can send the introductory information" do
     get '/api/v1/forecast?location=denver,co'
 
