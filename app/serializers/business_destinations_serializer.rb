02e984b866b0cac43a359635e1bf2f46e924b89c
oxalisviolacea <hannadavis09@gmail.com>
2021-01-20 09:11:37 -0700

    Change business destination serializer

---
+++ app/serializers/business_destinations_serializer.rb
@@ -0,0 +1,22 @@
+class BusinessDestinationsSerializer
+  def self.format_data(road_trip, business)
+    {
+      "data": {
+        "id": nil,
+        "type": "foodie",
+        "attributes": {
+          "end_location": road_trip.destination,
+          "travel_time": road_trip.travel_time,
+          "forecast": {
+            "summary": road_trip.conditions_at_eta,
+            "temperature": road_trip.temp_at_eta
+          },
+          "restaurant": {
+            "name": business[:name],
+            "address": business[:location][:address]
+          }
+        }
+      }
+    }
+  end
+end
