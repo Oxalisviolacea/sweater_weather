996d34f97c88e65e6e71d41e844f5d6400677056
oxalisviolacea <hannadavis09@gmail.com>
2021-01-19 18:20:07 -0700

    Add methods to find eta temp and conditions

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,20 +1,41 @@
 class RoadTrip
-  attr_reader :travel_time, :origin, :destination
-  
-  def initialize(origin, destination, trip_plan)
+  attr_reader :travel_time, :origin, :destination, :temp_at_eta, :conditions_at_eta
+
+  def initialize(origin, destination, trip_plan, forecast)
     @travel_time = format_travel_time(trip_plan[:route][:formattedTime])
     @origin = format_location(origin)
     @destination = format_location(destination)
+    @temp_at_eta = get_temp(forecast, trip_plan)
+    @conditions_at_eta = get_conditions(forecast, trip_plan)
   end
 
   def format_travel_time(travel_time)
-    travel_time.to_datetime.strftime("%H hours and %M minutes")
+    travel_time.to_datetime.strftime('%H hours %M minutes')
   end
 
   def format_location(location)
-    location.split(",")
+    location.split(',')
     location[1].upcase!
     location[0].capitalize!
-    location.join(", ")
+    location.gsub(',', ', ')
+  end
+
+  def eta(forecast, trip_plan)
+    drive_time_seconds = trip_plan[:route][:time]
+    no_zon_now = DateTime.now.strftime('%a, %d %b %Y %H:%M:%S').to_datetime
+    arrival_time = no_zon_now.advance(seconds: drive_time_seconds)
+    forecast.min_by do |weather_for_hour|
+      (arrival_time - weather_for_hour.time.to_datetime).abs
+    end
+  end
+
+  def get_temp(forecast,trip_plan)
+    forecast_for_eta = eta(forecast, trip_plan)
+    forecast_for_eta.temp.to_i
+  end
+
+  def get_conditions(forecast, trip_plan)
+    forecast_for_eta = eta(forecast, trip_plan)
+    forecast_for_eta.conditions
   end
 end
